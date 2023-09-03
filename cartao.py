2eec72676596c97f28d82b0766d05ee427b18e07
thiagoandrad <thpandrade@gmail.com>
2023-09-03 16:53:26 -0300

    Semana 1 - Tarefa 5 e 6

--- cartao.py
+++ cartao.py
@@ -1,14 +1,38 @@
 class Cartao:
     def __init__(self, numero, validade, cvv, limite, cliente):
-        self.cliente = cliente
-        self.limite = limite
-        self.cvv = cvv
-        self.validade = validade
-        self.numero = numero
-        self.status = 'ATIVO'
+        self.__cliente = cliente
+        self.__limite = limite
+        self.__cvv = cvv
+        self.__validade = validade
+        self.__numero = numero
+        self.__status = 'ATIVO'
+
+    @property
+    def numero(self):
+        return self.__numero
+
+    @property
+    def validade(self):
+        return self.__validade
+
+    @property
+    def cvv(self):
+        return self.__cvv
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @property
+    def cliente(self):
+        return self.__cliente
+
+    @property
+    def status(self):
+        return self.__status
 
     def cancela(self):
-        self.status = 'CANCELADO'
+        self.__status = 'CANCELADO'
 
     def ativa(self):
-        self.status = 'ATIVO'
+        self.__status = 'ATIVO'

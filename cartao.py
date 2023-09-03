8917c108f6fe8485c949c851d616c6e34c167eee
thiagoandrad <thpandrade@gmail.com>
2023-09-03 16:50:48 -0300

    Semana 1 - Tarefa 3

--- cartao.py
+++ cartao.py
@@ -1,2 +1,8 @@
 class Cartao:
-    pass
+    def __init__(self, numero, validade, cvv, limite, cliente):
+        self.cliente = cliente
+        self.limite = limite
+        self.cvv = cvv
+        self.validade = validade
+        self.numero = numero
+        self.status = 'ATIVO'
--- teste_cartao.py
+++ teste_cartao.py
@@ -1,7 +1,20 @@
 from cartao import Cartao
 
-visa = Cartao()
-mastercard = Cartao()
+visa = Cartao('1111 1111 1111 1111', '01/2031', '321', 1000.0, 'Steve Rogers')
+mastercard = Cartao('2222 2222 2222 2222', '05/2035', '789', 2000.0, 'Matt Murdock')
 
-print(visa)
-print(mastercard)
+print(visa.numero)
+print(visa.validade)
+print(visa.cvv)
+print(visa.limite)
+print(visa.cliente)
+print(visa.status)
+
+print()
+
+print(mastercard.numero)
+print(mastercard.validade)
+print(mastercard.cvv)
+print(mastercard.limite)
+print(mastercard.cliente)
+print(mastercard.status)

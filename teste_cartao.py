b01c071f9aa6b34c341cee359d3b1f1b88d62076
thiagoandrad <thpandrade@gmail.com>
2023-09-03 17:19:54 -0300

    Semana 3 - Tarefa 4

--- teste_cartao.py
+++ teste_cartao.py
@@ -1,7 +1,9 @@
+from datetime import date
 from model import Cartao
 
-visa = Cartao('1111 1111 1111 1111', '01/2031', '321', 1000.0, 'Steve Rogers')
+visa = Cartao('1111 1111 1111 1111', date(2023, 1, 31), '321', 1000.0, 'Steve Rogers')
 mastercard = Cartao('2222 2222 2222 2222', '05/2035', '789', 2000.0, 'Matt Murdock')
+
 
 print(visa.numero)
 print(visa.validade)
@@ -19,16 +21,21 @@ print(mastercard.limite)
 print(mastercard.cliente)
 print(mastercard.status)
 
+print()
+
 visa.cancela()
 mastercard.cancela()
 
 print(visa.status)
 print(mastercard.status)
 
+print()
+
 visa.limite = 5000.0
 mastercard.limite = 7500.0
 
 print(visa.limite)
 print(mastercard.limite)
+print()
 
-cartao_invalido = Cartao('3333 3333 3333 3333', '5/29', '887', 10000.0, 'Bruce Wayne')
+cartao_invalido = Cartao('3333 3333 3333 3333', date(2029, 5, 31), '887', 10000.0, 'Bruce Wayne')
--- teste_compra.py
+++ teste_compra.py
@@ -1,19 +1,20 @@
 from model import Compra, Cartao, CompraCredito
+from datetime import datetime, date
 
-visa = Cartao('1111 1111 1111 1111', '01/2031', '321', 1000.0, 'Steve Rogers')
+visa = Cartao('1111 1111 1111 1111', date(2031, 1, 31), '321', 1000.0, 'Steve Rogers')
 
-compra_farmacia = Compra(100.0, '01/01/2023 10:00:00', 'Farmácia Popular', 'Saúde', visa)
-compra_restaurante = Compra(89.9, '02/01/2023 12:15:00', 'Burguer King', 'Lazer', visa)
-compra_supermercado = Compra(475.5, '03/02/2023 07:05:05', 'Carrefour', 'Alimentação', visa)
+compra_farmacia = Compra(100.0, datetime(2023, 1, 1, 10, 0, 0), 'Farmácia Popular', 'Saúde', visa)
+compra_restaurante = Compra(89.9, datetime(2023, 1, 2, 12, 15, 0), 'Burguer King', 'Lazer', visa)
+compra_supermercado = Compra(475.5, datetime(2023, 2, 3, 7, 5, 5), 'Carrefour', 'Alimentação', visa)
 
 print(compra_farmacia)
 print(compra_restaurante)
 print(compra_supermercado)
 print()
 
-compra_amazon = CompraCredito(1000.0, '15/02/2023 19:46:17', 'Amazon', 'Casa', visa, 10)
-print(
-    f'Compra a crédito: {compra_amazon.valor} em {compra_amazon.quantidade_parcelas}x de {compra_amazon.valor_parcela}')
+compra_amazon = CompraCredito(1000.0, datetime(2023, 2, 15, 19, 46, 17), 'Amazon', 'Casa', visa, 10)
+print(f'Compra a crédito: {compra_amazon.valor} em {compra_amazon.quantidade_parcelas}x de {compra_amazon.valor_parcela}')
+print()
 
 fatura = [compra_farmacia, compra_restaurante, compra_supermercado, compra_amazon]
 total = 0

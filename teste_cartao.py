793a1cf7e3e5411ed4b5bebe3e99fcfae487a3a1
thiagoandrad <thpandrade@gmail.com>
2023-09-03 16:55:49 -0300

    Semana 2 - Tarefa 1

--- teste_cartao.py
+++ teste_cartao.py
@@ -1,4 +1,4 @@
-from cartao import Cartao
+from model import Cartao
 
 visa = Cartao('1111 1111 1111 1111', '01/2031', '321', 1000.0, 'Steve Rogers')
 mastercard = Cartao('2222 2222 2222 2222', '05/2035', '789', 2000.0, 'Matt Murdock')

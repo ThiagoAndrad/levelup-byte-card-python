2457b92bab8cd3c7b99602d26c5968fcac247528
thiagoandrad <thpandrade@gmail.com>
2023-10-22 23:12:59 -0300

    Semana 11 - Tarefa 5 e 6

--- use_cases.py
+++ use_cases.py
@@ -4,7 +4,9 @@ from datetime import datetime
 from random import randint
 
 from dateutil.relativedelta import relativedelta
+from sqlalchemy import select
 
+from database import db
 from model import Cartao, Compra
 
 
@@ -22,39 +24,29 @@ def define_validade_do_cartao():
     validade = date.today() + relativedelta(years=4, months=6, day=31)
     return validade
 
-
-cartao1 = Cartao(cria_numero_do_cartao(), date(2031, 1, 31), '321', 1000.0, 'Steve Rogers', id=1)
-cartao2 = Cartao(cria_numero_do_cartao(), date(2035, 5, 31), '789', 2000.0, 'Matt Murdock', id=2)
-cartao3 = Cartao(cria_numero_do_cartao(), date(2029, 5, 31), '887', 10000.0, 'Bruce Wayne', id=3)
-
-banco_cartoes = {
-    cartao1.id: cartao1,
-    cartao2.id: cartao2,
-    cartao3.id: cartao3
-}
-sequencia_ids = 4
 banco_compras = []
 
 
 def lista_cartoes():
-    return banco_cartoes.values()
+    comando = select(Cartao).order_by(Cartao.cliente, Cartao.numero)
+    resultado = db.session.scalars(comando)
+
+    return list(resultado)
 
 
-def pesquisa_cartao_por_id(id):
-    return banco_cartoes.get(id)
+def pesquisa_cartao_por_id(cartao_id):
+    return db.session.get(Cartao, cartao_id)
 
 
 def cadastra_cartao(cliente, limite):
-    global sequencia_ids, banco_cartoes
-
     numero = cria_numero_do_cartao()
     cvv = cria_cvv_do_cartao()
     validade = define_validade_do_cartao()
 
-    novo_cartao = Cartao(numero=numero, validade=validade, cvv=cvv, limite=limite, cliente=cliente, id=sequencia_ids)
-    banco_cartoes[novo_cartao.id] = novo_cartao
+    cartao = Cartao(numero=numero, validade=validade, cvv=cvv, limite=limite, cliente=cliente)
 
-    sequencia_ids += 1
+    db.session.add(cartao)
+    db.session.commit()
 
 
 def cadastra_compra(cartao_id, valor, categoria, estabelecimento):
@@ -85,12 +77,19 @@ def define_limite(cartao_id, limite):
     cartao = pesquisa_cartao_por_id(cartao_id)
     cartao.limite = limite
 
+    db.session.commit()
 
-def cancela_cartao(cartao_id):
+
+def altera_status_cartao(cartao_id, operacao):
     cartao = pesquisa_cartao_por_id(cartao_id)
-    cartao.cancela()
+    operacao(cartao)
+
+    db.session.commit()
 
 
 def ativa_cartao(cartao_id):
-    cartao = pesquisa_cartao_por_id(cartao_id)
-    cartao.ativa()
+    altera_status_cartao(cartao_id, lambda c: c.ativa())
+
+
+def cancela_cartao(cartao_id):
+    altera_status_cartao(cartao_id, lambda c: c.cancela())
